7898534f78ea9e5dc7e2d83e0faa45d10bcf52e3
Jorge Nardy <nardyjh@Jorges-MacBook-Pro.local>
2023-10-28 16:06:43 -0400

    api app complete

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# sqlalchemy-challenge
+# Climate Analysis and API Development
+
+## Overview
+This project involves conducting a climate analysis of Honolulu, Hawaii and developing a Flask API based on the analysis results. It includes two main parts: data analysis using Python and SQLAlchemy and creating a web API with Flask.
+
+## Part 1: Analyze and Explore the Climate Data
+In this part, we used Python, SQLAlchemy, Pandas, and Matplotlib to analyze and explore the climate database. The following steps were performed:
+
+1. Connect to the SQLite database using SQLAlchemy.
+2. Reflect the tables and save references to the "measurement" and "station" tables.
+3. Conducted a precipitation analysis:
+    - Found the most recent date in the dataset.
+    - Retrieved the previous 12 months of precipitation data.
+    - Loaded the query results into a Pandas DataFrame and plotted the results.
+    - Calculated summary statistics for the precipitation data.
+
+4. Conducted a station analysis:
+    - Calculated the total number of stations.
+    - Identified the most active station by listing stations and observation counts.
+    - Calculated the lowest, highest, and average temperatures for the most active station.
+    - Plotted temperature observations as a histogram.
+
+## Part 2: Design Climate App
+In this part, we designed a Flask API based on the queries developed in Part 1. The API offers several routes to access climate data:
+
+- `/api/v1.0/precipitation`: Returns the last 12 months of precipitation data as a JSON dictionary.
+- `/api/v1.0/stations`: Returns a list of stations from the dataset as a JSON list.
+- `/api/v1.0/tobs`: Queries the dates and temperature observations of the most active station for the previous year and returns a JSON list of temperature observations.
+- `/api/v1.0/<start>`: Accepts a start date as a parameter and returns JSON data with the minimum, average, and maximum temperatures calculated from the start date to the end of the dataset.
+- `/api/v1.0/<start>/<end>`: Accepts start and end dates as parameters and returns JSON data with the temperature statistics calculated for the specified date range.
+
+## Project Structure
+- `climate-starter.ipynb`: Jupyter Notebook containing the initial data analysis.
+- `app.py`: Python script that defines the Flask application and API routes.
+- `Resources/`: Directory containing the CSV files and SQLite database file (`hawaii.sqlite`) used for the analysis.
+
+## Dependencies
+- Python
+- Flask
+- SQLAlchemy
+- Pandas
+- Matplotlib
+
+## Usage
+1. Clone this repository to your local machine.
+2. Ensure you have the necessary dependencies installed.
+3. Run `app.py` to start the Flask application.
+4. Access the provided routes to retrieve climate data in JSON format.
+
+## Closing Notes
+This project demonstrates how to perform climate analysis and create a web API using Flask, providing easy access to climate data. Feel free to explore the provided routes and enhance the functionality as needed for your specific use case.
+
+For any questions or improvements, please contact Jorge Nardy.
+
+Enjoy your exploration of Honolulu's climate data!
+
+## Acknowledgments 
+Data provided by University of Toronto. 
--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,158 @@
 # Import the dependencies.
-
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+import numpy as np
+from datetime import datetime, timedelta
+import datetime as dt
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
+session = Session(engine)
 
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def homepage():
+    return (
+        f"Welcome to the Climate App API!<br/>"
+        f"Available Routes:<br/>"
+        f"<a href='/api/v1.0/precipitation'>/api/v1.0/precipitation</a><br/>"
+        f"<a href='/api/v1.0/stations'>/api/v1.0/stations</a><br/>"
+        f"<a href='/api/v1.0/tobs'>/api/v1.0/tobs</a><br/>"
+        f"/api/v1.0/&lt;start&gt; (replace &lt;start&gt; with a date)<br/>"
+        f"/api/v1.0/&lt;start&gt;/&lt;end&gt; (replace &lt;start&gt; and &lt;end&gt; with dates)"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Query the last 12 months of precipitation data and convert to dictionary
+    # Calculate date
+    most_recent_date = session.query(func.max(measurement.date)).scalar()
+    most_recent_date = datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - timedelta(days=365)
+
+    #Query precipitation for last 12 months
+    results = session.query(measurement.date, measurement.prcp).filter(measurement.date >= one_year_ago).all()
+    
+    # Convert results into dictionary
+    precipitation_data = {}
+    for date, prcp in results:
+        precipitation_data[date] = prcp 
+        
+    #Return JSON representation of data
+    return jsonify(precipitation_data)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Query the list of stations and return as JSON
+    results = session.query(station.station).all()
+
+    #Convert results into Python list
+    station_data = [result[0] for result in results]
+
+    #Return JSON
+    return jsonify(station_data)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Query temperature observations for the most active station for the previous year and return as JSON
+    most_recent_date = session.query(func.max(measurement.date)).scalar()
+    most_recent_date = datetime.strptime(most_recent_date, '%Y-%m-%d')
+    one_year_ago = most_recent_date - timedelta(days=365)
+
+    #Find most active station
+    most_active_station = session.query(measurement.station, func.count(measurement.station)) \
+    .group_by(measurement.station) \
+    .order_by(func.count(measurement.station).desc()) \
+    .first()[0]
+
+    #Query temperature observations for most active station in last year
+    results = session.query(measurement.date, measurement.tobs) \
+    .filter(measurement.station == most_active_station) \
+    .filter(measurement.date >= one_year_ago) \
+    .all()
+
+    #Convert results to a list of dictionaries 
+    tobs_data = []
+    for date, tobs in results:
+        tobs_data.append({
+            "date": date,
+            "tobs": tobs})
+
+    #Return JSON
+    return jsonify(tobs_data)
+
+
+@app.route("/api/v1.0/<start>")
+def start_date(start):
+    # Convert the start date string to a datetime object
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
+
+    # Query to calculate TMIN, TAVG, and TMAX for dates greater than or equal to the start date
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs))\
+        .filter(measurement.date >= start_date).all()
+
+    # Unpack the results
+    tmin, tavg, tmax = results[0]
+
+    # Create a dictionary with the results
+    stats_data = {
+        "TMIN": tmin,
+        "TAVG": tavg,
+        "TMAX": tmax
+    }
+
+    #Return JSON
+    return jsonify(stats_data)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_date(start, end):
+    # Convert to datetime objects
+    start_date = dt.datetime.strptime(start, "%Y-%m-%d")
+    end_date = dt.datetime.strptime(end, "%Y-%m-%d")
+
+    # Query database to get temperature statistics for date range
+    results = session.query(func.min(measurement.tobs), func.avg(measurement.tobs), func.max(measurement.tobs))\
+        .filter(measurement.date >= start_date, measurement.date <= end_date).all()
+
+    # Unpack the results
+    (min_temp, avg_temp, max_temp) = results[0]
+
+    # Create a dictionary of results
+    stats_data = {
+        "TMIN": min_temp,
+        "TAVG": avg_temp,
+        "TMAX": max_temp
+    }
+
+    #Return JSON
+    return jsonify(stats_data)
+
